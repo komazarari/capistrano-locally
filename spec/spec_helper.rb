4a789e20b9568d46da7145178fe155f3511b9667
Takuto Komazaki <komazarari@gmail.com>
2019-02-14 10:08:46 +0900

    Add specs

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,2 +1,6 @@
-$LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
+$LOAD_PATH.unshift File.expand_path('../lib', __dir__)
+
+require 'capistrano'
+require 'singleton'
+require 'sshkit'
 require 'capistrano/locally'
